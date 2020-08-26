f5cbeb03b5fdb12c222b8cb9661138d4f4e9b0f3
Andres Chaves <gachavesb@correo.udistrital.edu.co>
2020-08-25 21:20:33 -0500

    Update tkinter1.py

--- Tarea1/tkinter1.py
+++ Tarea1/tkinter1.py
@@ -5,19 +5,19 @@ from tkinter import messagebox
 def DefInformatica():
 ####    messagebox.showinfo("XXX",message = "!!")
     lb2 = Label(ventana, image=imagenbt).place(x=0,y=0)
-    
+
 def Matematica():
     messagebox.showinfo("MATEMATICAS",message = "Las  Matemáticas  ofrecen  herramientas  que  permiten  analizar, evaluar   y   gestionar   dichas   amenazas   con  el   objetivo   deminimizar su impacto:Algoritmos   criptográficos   para   proteger   la   información (confidencialidad, integridad, autenticidad, etc.)Modelos  matemáticos  para  detectar,  evaluar  y  gestionar potenciales amenazas en la red. Modelos  matemáticos  para  el  análisis  y  comportamiento  de redes terroristas.")
-    
+
 
 def Electronica():
     messagebox.showinfo("ELECTRONICA",message = "Es una relacion muy importante ya que es el medio fisico de la informatica, tanto su relacion estrechamente que a medida que la electronica mejora, reduciendo tamaño y precio y aumentando su calidad, asi se consiguen ordenadores rapidos, economicos y eficaces")
-    
+
 
 def Fisica():
-    messagebox.showinfo("FISICA",message = "!!")
-    
-    
+    messagebox.showinfo("FISICA",message = "De acuerdo al gran físico, recientemente fallecido, Rolf Landauer “information is physical”, la información es un proceso físico pues algo debe transmitirse por lo que debe ser algo medible tanto antes de enviarse, como también al procesarse o al ser recibida; independientemente de que se transmitan,ondas electromagnéticas, ondas mecánicas o señales de humo, el hecho es que algo que va a transmitirse debe ser medido y cuantificado con precisión matemática y rigor lógico, es decir, debe ser una cantidad física")
+
+
 ventana = Tk()
 ventana.geometry('600x600')
 ventana.title("Tarea 1 Informatica")
