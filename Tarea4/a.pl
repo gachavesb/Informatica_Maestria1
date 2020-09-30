835da827ab8cc457fcf9aaa3ec458e62ccb7f612
Andres Chaves <gachavesb@correo.udistrital.edu.co>
2020-09-30 11:14:23 -0500

    Update a.pl

--- Tarea4/a.pl
+++ Tarea4/a.pl
@@ -16,10 +16,27 @@ municipios_cundinamarca('Sasaima').
 municipios_cundinamarca('Choachi').
 municipios_cundinamarca('Silvania').
 municipios_cundinamarca('Ubate').
+cercania_a_Bogota('Soacha','5').
+cercania_a_Bogota('Girardot','100').-0oi9>A+
+.540
+BN 
+0V CVNM,
+cercania_a_Bogota('Sasaima','80').
+cercania_a_Bogota('Zipaquira','30').
+cercania_a_Bogota('La Vega','50').
+temperatura_pueblo_engrados('Soacha','16').
+temperatura_pueblo_engrados('Chia','16').
+temperatura_pueblo_engrados('Girardot','25').
+temperatura_pueblo_engrados('Fusa','22').
+temperatura_pueblo_engrados('Ubate','10').
+temperatura_pueblo_engrados('La Vega','23').
+temperatura_pueblo_engrados('Choachi','16').
+viajar_cerca_cundinamarca(X,Y):-
+	X < 31 -> Y = muy_cercanos; 
+	X < 60 -> Y = cercanos;
+	true -> Y = lejos.
 
-Limita_de_Bogota('Soacha')
-Limita_de_Bogota('Cota')
-Limita_de_Bogota('Chia')
-Limita_de_Bogota('Funza')
-Limita_de_Bogota('Madrid')
+muy_cercanos(X):-cercania_a_bogota(X,Z),viajar_cerca_cundinamarca(Z,W),W=muy_cercanos.
+
+
 
