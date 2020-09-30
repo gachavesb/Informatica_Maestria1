916ee528882dc83b57343d7710e9fd7e1f683c44
Andres Chaves <gachavesb@correo.udistrital.edu.co>
2020-09-30 07:08:26 -0500

    Update a.pl

--- Tarea4/a.pl
+++ Tarea4/a.pl
@@ -1,2 +1,20 @@
-es_un_pais('Colombia').
-es_un_pais('Brasil').
+municipios_cundinamarca('Chia').
+municipios_cundinamarca('Soacha').
+municipios_cundinamarca('Cota').
+municipios_cundinamarca('Funza').
+municipios_cundinamarca('Mosquera').
+municipios_cundinamarca('Madrid').
+municipios_cundinamarca('El rosal').
+municipios_cundinamarca('Tenjo').
+municipios_cundinamarca('Cajica').
+municipios_cundinamarca('Zipaquira').
+municipios_cundinamarca('La Vega').
+municipios_cundinamarca('Girardot').
+municipios_cundinamarca('Fusagasuga').
+municipios_cundinamarca('Tocaima').
+municipios_cundinamarca('Sasaima').
+municipios_cundinamarca('Choachi').
+municipios_cundinamarca('Silvania').
+municipios_cundinamarca('Ubate').
+
+
