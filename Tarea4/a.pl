e12d23d2dd912e52f609cb8dde818f3d67f77811
Andres Chaves <gachavesb@correo.udistrital.edu.co>
2020-09-30 11:31:42 -0500

    Update a.pl

--- Tarea4/a.pl
+++ Tarea4/a.pl
@@ -16,27 +16,29 @@ municipios_cundinamarca('Sasaima').
 municipios_cundinamarca('Choachi').
 municipios_cundinamarca('Silvania').
 municipios_cundinamarca('Ubate').
-cercania_a_Bogota('Soacha','5').
-cercania_a_Bogota('Girardot','100').-0oi9>A+
-.540
-BN 
-0V CVNM,
-cercania_a_Bogota('Sasaima','80').
-cercania_a_Bogota('Zipaquira','30').
-cercania_a_Bogota('La Vega','50').
-temperatura_pueblo_engrados('Soacha','16').
-temperatura_pueblo_engrados('Chia','16').
-temperatura_pueblo_engrados('Girardot','25').
-temperatura_pueblo_engrados('Fusa','22').
-temperatura_pueblo_engrados('Ubate','10').
-temperatura_pueblo_engrados('La Vega','23').
-temperatura_pueblo_engrados('Choachi','16').
+cercania_a_bogota('Soacha',5).
+cercania_a_bogota('Girardot',100).
+cercania_a_bogota('Sasaima',80).
+cercania_a_bogota('Zipaquira',30).
+cercania_a_bogota('La Vega',50).
+temperatura_pueblo_engrados('Soacha',16).
+temperatura_pueblo_engrados('Chia',16).
+temperatura_pueblo_engrados('Girardot',25).
+temperatura_pueblo_engrados('Fusa',22).
+temperatura_pueblo_engrados('Ubate',10).
+temperatura_pueblo_engrados('La Vega',23).
+temperatura_pueblo_engrados('Choachi',16).
+
 viajar_cerca_cundinamarca(X,Y):-
 	X < 31 -> Y = muy_cercanos; 
 	X < 60 -> Y = cercanos;
 	true -> Y = lejos.
 
 muy_cercanos(X):-cercania_a_bogota(X,Z),viajar_cerca_cundinamarca(Z,W),W=muy_cercanos.
+cercanos(X):-cercania_a_bogota(X,Z),viajar_cerca_cundinamarca(Z,W),W=cercanos.
+lejanos(X):-cercania_a_bogota(X,Z),viajar_cerca_cundinamarca(Z,W),W=lejos.
+
+
 
 
 
